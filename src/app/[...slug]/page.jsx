f3bcb17df32dec5e39bc297ef2a570dbdab6e716
RavenKape02 <jfcaduyac@up.edu.ph>
2026-04-15 06:50:39 +0800

    Fix: fix slug

--- src/app/[...slug]/page.jsx
+++ src/app/[...slug]/page.jsx
@@ -1,5 +1,9 @@
 import { ClientOnly } from "../client-only";
 
+export function generateStaticParams() {
+  return [{ slug: ["favorites"] }];
+}
+
 export default function CatchAllPage() {
   return <ClientOnly />;
 }
